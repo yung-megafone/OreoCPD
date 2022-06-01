04f2e88f62021434851a8d83c154a6e080de39b7
Branden <61205646+banasenes@users.noreply.github.com>
2022-06-01 03:52:08 -0500

    Update and rename start.py to main.py
    Fixed some bugs and added demo and shutdown programs

--- OreoCPDv1A/main.py
+++ OreoCPDv1A/main.py
@@ -8,6 +8,7 @@
                                     ##########   Setup   ##########
                             ############     General Setup     ############
 import time
+from turtle import clear
 import RPi.GPIO as GPIO
 from tqdm import tqdm , trange
 from tqdm.notebook import tqdm_notebook
@@ -25,39 +26,46 @@ def clear_screen():
 ##################################################################
 
 ###     Menu and Program     ###
-    ###Providing an alternate loopback point to not reset modules on bad input ###
+    ### Providing an alternate loopback point to not reset modules on bad input ###
 def main():
     clear_screen()
     option = 0 # set the input variable 'option' to 0
         ### Loop thru the menu items and when one is selected, execute the underlying commands. Otherwise restart the program with an error message
-    while option < 9:
-        print("[1] Default\n\n [2] Daily\n\n [3] Dark Car\n")
+    while 1:
+        print(" [1] Demo\n\n [2] Default\n\n [3] Daily\n\n [4] Dark Car\n\n ")
         print("")
         option = int(input("Please enter selection:  "))
 
-        if option == 1:
+        if option == 1: # demo #
             clear_screen()
-            print("Default preset loading...\n")
+            program.demo()
+            # sleep to ensure all relays are switched and devices are recieving power before continuing
+            time.sleep(2)
+
+
+        elif option == 2:   # defualt #
+            clear_screen()
             program.default()
-            time.sleep(2) # sleep to ensure all relays are switched and devices are recieving power
-            print("All devices ready!")
-            print("##################")
+            time.sleep(2)
 
-        elif option == 2:
+
+        elif option == 3:   # daily #
             clear_screen()
-            print("Daily driver preset loading...\n")
             program.daily_driver()
-            time.sleep(2) # sleep to ensure all relays are switched and devices are recieving power
-            print("Essential devices ready!")
-            print("########################")
+            time.sleep(2)
 
-        elif option == 3:
+
+        elif option == 4:   # stealth #
             clear_screen()
-            print("Stealth preset loading...\n")
             program.dark_car()
-            time.sleep(2) # sleep to ensure all relays are switched and devices are recieving power
-            print("Everything but music is deactivated, be safe!")
-            print("#############################################")
+            time.sleep(2)
+
+        
+        elif option == 5:
+            clear_screen()
+            program.all_off()
+            time.sleep(2)
+
             ###     End the program    ###
         elif option == 9:
             print("Please make sure to disable the main power switch before parking for long periods of time!")
@@ -73,7 +81,7 @@ def main():
 def oreocpd():
     print("System Starting()")
     print("")
-    default() # load the default profile
+    program.default() # load the default profile
     time.sleep(1)
     main()
 
